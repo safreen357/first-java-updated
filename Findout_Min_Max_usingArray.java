f03b803121ca6b30cae29faf32dbe927dfdb5484
safreen357 <91469380+safreen357@users.noreply.github.com>
2022-01-29 18:29:26 +0530

    Add files via upload
    array min man by user input values

---
+++ Findout_Min_Max_usingArray.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Findout_Min_Max_usingArray {
+    static void min(int arr[])
+    {
+        int min=arr[0];
+        int max=arr[0];
+        for(int i=1;i<arr.length;i++)
+        {
+
+            if(min>arr[i])
+            {
+                min=arr[i];
+                System.out.println("MinimumElement " +min);
+            }
+
+            else if(max<arr[i])
+            {
+                max=arr[i];
+
+                System.out.println("MaximumElement " +max);
+            }
+        }
+    }
+    public static void main (String[] args) {
+
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of elements you want to store: ");
+        //reading the number of elements from the that we want to enter
+        n=sc.nextInt();
+        //creates an array in the memory of length 10
+        int[] array = new int[10];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0; i<n; i++)
+        {
+            //reading array elements from the user
+            array[i]=sc.nextInt();
+        }
+        System.out.println("Array elements are: ");
+        // accessing array elements using the for loop
+        for (int i=0; i<n; i++)
+        {
+            System.out.println(array[i]);
+        }
+        min(array);//passing array to method
+    }
+
+}
